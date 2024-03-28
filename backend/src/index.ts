357b2109b627320e74673fd93911d078e2ff63d3
kentrellmalone <kentrellmalone9015@gmail.com>
2024-03-28 03:47:08 -0700

    My Hotels was added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/myhotels", myHotelRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 
 app.get("*", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
